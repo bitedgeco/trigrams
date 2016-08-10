863f0bbc60636d5609d77c45ee280a67b77e6532
James <canning.jimmy@gmail.com>
2016-08-10 13:56:37 -0700

    Set up first function and test

--- src/test_trigrams.py
+++ src/test_trigrams.py
@@ -1,1 +1,11 @@
 # _*_ coding: utf-8 _*_
+import pytest
+
+SHERLOCK_SMALL = "One night--it was on the twentieth of March, 1888--I was returning from a journey to a patient (for I had now returned to civil practice), when my way led me through Baker Street. As I passed the well-remembered door, which must always be associated"
+
+def test_get_text_data():
+    from trigrams import get_text_data
+    assert get_text_data(sherlock_small.txt) == SHERLOCK_SMALL
+ 
+
+
--- src/trigrams.py
+++ src/trigrams.py
@@ -1,1 +1,10 @@
 # _*_ coding: utf-8 _*_
+
+import io
+
+
+def get_text_data(text):
+    input_file = io.open(text, encoding="utf-8")
+    text_data = input_file.read()
+    input_file.close()
+    return text_data
