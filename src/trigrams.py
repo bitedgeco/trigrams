461dbb68381ebd3a69fa69ad8bfc8711236bb0a7
tanyaweaver <tanya83@uw.edu>
2016-08-10 14:51:13 -0700

    defined functions 1. to read a text file and return a string, 2. to split a string and return a list, 3. to create a dictionari from the list, using i and i + 1 words as a key and i + 2 word as a value; tests for all 3 are passing

--- src/trigrams.py
+++ src/trigrams.py
@@ -4,7 +4,22 @@ import io
 
 
 def get_text_data(text):
+    """Return text from a file as a string"""
     input_file = io.open(text, encoding="utf-8")
     text_data = input_file.read()
     input_file.close()
     return text_data
+
+
+def split_string_into_words(text_string):
+    split_string = text_string.split()
+    return split_string
+
+
+def make_dict_from_list(word_list):
+    kata_dict = {}
+    for i in range(0,len(word_list) - 2, 3):
+        kata_key = word_list[i] + ' ' + word_list[i + 1]
+        kata_value = word_list[i + 2]
+        kata_dict[kata_key] = kata_value
+    return kata_dict
