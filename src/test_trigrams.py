ca9d1bf5da937f2d1d91d45d4797c2971d7d26b4
tanyaweaver <tanya83@uw.edu>
2016-08-10 23:10:01 -0700

    function main generates text using the dcreated dictionary

--- src/test_trigrams.py
+++ src/test_trigrams.py
@@ -1,18 +1,23 @@
 # _*_ coding: utf-8 _*_
 import pytest
-
-SHERLOCK_SMALL = "One night--it was on the twentieth of March, 1888--I was returning from a journey to a patient (for I had now returned to civil practice), when my way led me through Baker Street. As I passed the well-remembered door, which must always be associated\n"
+import io
+SHERLOCK_SMALL_LIST = ["One",  "night--it", "was", "on", "the", "twentieth", "of", "March,", "1888--I"]
 
 def test_get_text_data():
     from trigrams import get_text_data
-    assert get_text_data('src/sherlock_small.txt') == SHERLOCK_SMALL
+    assert get_text_data('src/sherlock_small.txt') == SHERLOCK_SMALL_LIST
  
-
-def test_split_string_into_words():
-    from trigrams import split_string_into_words
-    assert split_string_into_words('test word piece') == ['test', 'word', 'piece']
-
 
 def test_make_dict_from_list():
     from trigrams import make_dict_from_list
-    assert make_dict_from_list(['key1a', 'key1b', 'value1', 'key2a', 'key2b', 'value2', 'key1a', 'key1b', 'value3']) == {"key1a key1b": ["value1", "value3"], "key2a key2b": ["value2"]}
+    assert make_dict_from_list(['test', 'word', 'piece', 'test', 'word', 'hat']) == {"test word": ["piece", "hat"], "word piece": ["test"], "piece test": ["word"]}
+
+
+def test_make_text_from_dict():
+    from trigrams import make_text_from_dict
+    assert make_text_from_dict('test word', {"test word": ["piece"], "word piece": ["test"], "piece test": ["word"]}, 10) == "test word piece test word piece test word piece test"
+
+
+def test_main_function():
+    from trigrams import main_function
+    assert main_function('src/sherlock_small.txt', 5) == 'One night--it was on the'
--- src/trigrams.py
+++ src/trigrams.py
@@ -8,17 +8,13 @@ def get_text_data(text):
     input_file = io.open(text, encoding="utf-8")
     text_data = input_file.read()
     input_file.close()
-    return text_data
-
-
-def split_string_into_words(text_string):
-    split_string = text_string.split()
-    return split_string
+    text_data_as_list =text_data.split()
+    return text_data_as_list
 
 
 def make_dict_from_list(word_list):
     kata_dict = {}
-    for i in range(0, len(word_list) - 2, 3):
+    for i in range(len(word_list) - 2):
         kata_key = word_list[i] + ' ' + word_list[i + 1]
         if kata_key in kata_dict.keys():
              kata_value = kata_dict.get(kata_key)
@@ -28,3 +24,26 @@ def make_dict_from_list(word_list):
         kata_value.append(word_list[i + 2])
         kata_dict[kata_key] = kata_value
     return kata_dict
+
+def make_text_from_dict(first_word_pair, created_dictionary, new_text_length):
+    created_text = first_word_pair
+    for i in range (new_text_length - 2):
+        next_word_list = created_dictionary.get(first_word_pair)
+        if next_word_list:
+            next_word = next_word_list[0]
+            created_text = created_text + " " + next_word
+            created_text_to_list = created_text.split()
+            first_word_pair = str(created_text_to_list[-2]) + ' ' + str(created_text_to_list[-1])
+        else:
+            print('stopped')
+    return created_text
+
+
+def main_function(source_path, word_count):
+    text = source_path
+    text_data_as_list = get_text_data(text)
+    first_pair = get_text_data(text)[0] + ' ' + get_text_data(text)[1]
+    created_dictionary = make_dict_from_list(text_data_as_list)
+    created_text = make_text_from_dict(first_pair, created_dictionary, word_count)
+    print(created_text)
+    return created_text
